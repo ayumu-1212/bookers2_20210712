19b5e550ad8b5a483e6ec293922d142b62925352
ayumu-1212 <ayumuabe1434@gmail.com>
2021-07-21 19:37:02 +0000

    :sparkles: いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :books
+  post "favorites/:user_id/:book_id" => "favorites#create", as: "favorites_create"
+  delete "favorites/:user_id/:book_id" => "favorites#destroy", as: "favorites_destroy"
   root to: "homes#top"
 end
